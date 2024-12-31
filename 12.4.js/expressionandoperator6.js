bcdeae7d1e0f3c57c64e67992b6ee421e6b5c0ba
Ghanshyam Patel <135823842+Ghanshyam0001@users.noreply.github.com>
2024-12-31 22:14:42 +0530

    Add files via upload

---
+++ 12.4.js/expressionandoperator6.js
@@ -0,0 +1,45 @@
+// operator in js
+let a = 35;
+let b = 45;
+console.log("a+b",a+b)
+console.log("a-b",a-b)
+console.log("a%b",a%b)
+console.log("a*b",a*b)
+console.log("a**b",a**b) // 35*35*35..(45 times)
+console.log("a/b",a/b)
+console.log("a++ = ",a++)
+console.log("a",a)
+console.log("++a  = ",++a)
+console.log("a-- = ",a--)
+console.log("--a =",--a)
+// Assigment operator
+// a +=5 same as a = a+5
+let assigment = 3;
+assigment += 2
+console.log(assigment)
+assigment -= 2
+console.log(assigment)
+assigment *= 2
+console.log(assigment)
+assigment /= 2
+console.log(assigment)
+
+// comperision operator
+let comp1 =5;
+let comp2 = "5";
+console.log("comp1 == comp2",comp1 == comp2) 
+console.log("comp1 != comp2",comp1 != comp2) 
+console.log("comp1 === comp2",comp1 === comp2) // value is same but second value is string so its False
+console.log("comp1 !== comp2",comp1 !== comp2) 
+console.log("comp1 < comp2",comp1 < comp2)
+console.log("comp1 <= comp2",comp1 <= comp2)  
+
+// logical operator
+let x = 12;
+let y = 13;
+console.log(x<y && x!=y)
+console.log(x<y || x ==y)
+console.log(!false)
+
+// 7+6  = 13  [+ is operator ][7 and 6 is oprand ][13 is result]
+
---
+++ 12.4.js/primitivedatatype4.js
@@ -0,0 +1,29 @@
+// nn bb ss u
+// let a = null;
+// let b = 765;
+let c = true; // can also be false
+let d = bigInt("456")
+// let e = "abc"
+// let f = Symbol("i am symbol")
+// let g = undefined
+console.log(d)
+// console.log(typeof c)
+
+
+// // object in JS and python called dictonary
+// const item = {
+//     "ghanshyam":true,
+//      "rohan":false,
+//      "hiren":67,
+//     "golpal":undefined
+// }
+// console.log(item["ghanshyam"])
+// console.log(item["ghansh"])
+// console.log(item["gopal"])
+// console.log(item["undefined"])
+
+const mm = {
+    "abc": aa,
+    "def":22
+}
+console.log(mm["abc"])
